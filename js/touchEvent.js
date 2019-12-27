119f5d6700c18c4d60c8483706f6ab05ea74ecbb
lsg <luoshaogang@youlanw.com>
2019-12-27 21:06:30 +0800

    25

--- js/touchEvent.js
+++ js/touchEvent.js
@@ -7,16 +7,19 @@ $('.front,.back,.right,.left,.top,.bottom').bind('click touchstart', function ()
         $('.right').css('transform', 'rotateY(-270deg) translateZ(100px) translateX(100px)');
         $('.left').css('transform', 'rotateY(270deg) translateZ(100px) translateX(-100px)');
         $('.top').css('transform', 'rotateX(-270deg) translateZ(100px) translateY(-100px)');
-        $('.bottom').css('transform', 'rotateX(270deg) translateZ(100px) translateY(100px)');
+        $('.bottom').css('transform', 'rotateX(270deg) translateZ(100px) translateY(100px)'); 
+        alert($(this)[0].outerHTML)       
         tsFlag = false;
     } else {   
-        alert(2)     
-        $('.front').removeAttr('style');        
-        $('.back').removeAttr('style');
-        $('.right').removeAttr('style');
-        $('.left').removeAttr('style');
-        $('.top').removeAttr('style');
-        $('.bottom').removeAttr('style');
+        alert(2) 
+        $(this).removeAttr('style');    
+        // $('.front').removeAttr('style');        
+        // $('.back').removeAttr('style');
+        // $('.right').removeAttr('style');
+        // $('.left').removeAttr('style');
+        // $('.top').removeAttr('style');
+        // $('.bottom').removeAttr('style');
+        alert($(this)[0].outerHTML)
         tsFlag = true;
     }
 })
