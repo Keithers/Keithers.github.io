8b563b510ea118e6cfd6d3c8f3676c100f9a19f3
lsg <luoshaogang@youlanw.com>
2019-12-27 20:28:01 +0800

    11

--- js/touchEvent.js
+++ js/touchEvent.js
@@ -1,16 +1,16 @@
 var tsFlag = true;
 $('.wrap').bind('touchstart', function () {
-    if (tsFlag) {
-        tsFlag = false;
+    if (tsFlag) {       
         $('.front').css('transform', 'translateZ(200px)');
         $('.back').css('transform', 'translateZ(-200px) rotateY(180deg)');
         $('.right').css('transform', 'rotateY(-270deg) translateZ(100px) translateX(100px)');
         $('.left').css('transform', 'rotateY(270deg) translateZ(100px) translateX(-100px)');
         $('.top').css('transform', 'rotateX(-270deg) translateZ(100px) translateY(-100px)');
         $('.bottom').css('transform', 'rotateX(270deg) translateZ(100px) translateY(100px)');
-    } else {
-        tsFlag = true;
+        tsFlag = false;
+    } else {        
         $('.front,.back,.right,.left,.top,.bottom').removeAttr('style')
+        tsFlag = true;
     }
 
 })
