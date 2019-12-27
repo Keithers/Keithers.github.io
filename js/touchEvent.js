5b0fd0b0fa35ca97eb920e8a2507df26b00027fe
lsg <luoshaogang@youlanw.com>
2019-12-27 21:31:47 +0800

    youhau

--- js/touchEvent.js
+++ js/touchEvent.js
@@ -1,24 +1,20 @@
 var tsFlag = true;
 $('html').bind('click touchstart', function () {
-    if (tsFlag) {
-        alert(1)       
+    if (tsFlag) {              
         $('.front').css('transform', 'translateZ(200px)');
         $('.back').css('transform', 'translateZ(-200px) rotateY(180deg)');
         $('.right').css('transform', 'rotateY(-270deg) translateZ(100px) translateX(100px)');
         $('.left').css('transform', 'rotateY(270deg) translateZ(100px) translateX(-100px)');
         $('.top').css('transform', 'rotateX(-270deg) translateZ(100px) translateY(-100px)');
-        $('.bottom').css('transform', 'rotateX(270deg) translateZ(100px) translateY(100px)'); 
-        alert($(this)[0].outerHTML)       
+        $('.bottom').css('transform', 'rotateX(270deg) translateZ(100px) translateY(100px)');               
         tsFlag = false;
-    } else {   
-        alert(2)        
+    } else {           
         $('.front').removeAttr('style');        
         $('.back').removeAttr('style');
         $('.right').removeAttr('style');
         $('.left').removeAttr('style');
         $('.top').removeAttr('style');
-        $('.bottom').removeAttr('style');
-        alert($(this)[0].outerHTML)
+        $('.bottom').removeAttr('style');        
         tsFlag = true;
     }
 })
