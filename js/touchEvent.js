4e135877832d2bd5275f9635e413dff54ce0136b
lsg <luoshaogang@youlanw.com>
2019-12-27 20:19:59 +0800

    updates

--- js/touchEvent.js
+++ js/touchEvent.js
@@ -1,32 +1,16 @@
-// .wrap:hover .front {
-//     transform: translateZ(200px);
-// }
+var tsFlag = true;
+$('.wrap').bind('touchstart', function () {
+    if (tsFlag) {
+        tsFlag = false;
+        $('.front').css('transform', 'translateZ(200px)');
+        $('.back').css('transform', 'translateZ(-200px) rotateY(180deg)');
+        $('.right').css('transform', 'rotateY(-270deg) translateZ(100px) translateX(100px)');
+        $('.left').css('transform', 'rotateY(270deg) translateZ(100px) translateX(-100px)');
+        $('.top').css('transform', 'rotateX(-270deg) translateZ(100px) translateY(-100px)');
+        $('.bottom').css('transform', 'rotateX(270deg) translateZ(100px) translateY(100px)');
+    } else {
+        tsFlag = true;
+        $('.front,.back,.right,.left,.top,.bottom').removeAttr('style')
+    }
 
-// .wrap:hover .back {
-//     transform: translateZ(-200px) rotateY(180deg);
-// }
-
-// .wrap:hover .right {
-//     transform: rotateY(-270deg) translateZ(100px) translateX(100px);
-// }
-
-// .wrap:hover .left {
-//     transform: rotateY(270deg) translateZ(100px) translateX(-100px);
-// }
-
-// .wrap:hover .top {
-//     transform: rotateX(-270deg) translateZ(100px) translateY(-100px);
-// }
-
-// .wrap:hover .bottom {
-//     transform: rotateX(270deg) translateZ(100px) translateY(100px);
-// }
-
-$('.wrap').bind('touchstart',function(){
-   $('.front').css('transform','translateZ(200px)');
-   $('.back').css('transform','translateZ(-200px) rotateY(180deg)');
-   $('.right').css('transform','rotateY(-270deg) translateZ(100px) translateX(100px)');
-   $('.left').css('transform', 'rotateY(270deg) translateZ(100px) translateX(-100px)');
-   $('.top').css('transform', 'rotateX(-270deg) translateZ(100px) translateY(-100px)');
-   $('.bottom').css('transform','rotateX(270deg) translateZ(100px) translateY(100px)');
 })
