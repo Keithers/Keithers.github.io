b288cac2b7cebac56e7d8f911712b17d59666e08
lsg <luoshaogang@youlanw.com>
2019-12-27 20:03:27 +0800

    1

--- js/touchEvent.js
+++ js/touchEvent.js
@@ -23,5 +23,10 @@
 // }
 
 $('.wrap').bind('touchstart',function(){
-    alert(520)
+   $('.front').css('transform','translateZ(200px)');
+   $('.back').css('transform','translateZ(-200px) rotateY(180deg)');
+   $('.right').css('transform','rotateY(-270deg) translateZ(100px) translateX(100px)');
+   $('.left').css('transform', 'rotateY(270deg) translateZ(100px) translateX(-100px)');
+   $('.top').css('transform', 'rotateX(-270deg) translateZ(100px) translateY(-100px)');
+   $('.bottom').css('transform','rotateX(270deg) translateZ(100px) translateY(100px)');
 })
