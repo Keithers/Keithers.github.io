d6be8010a2376e41dad3075f7b2fa91c9af8ea08
lsg <luoshaogang@youlanw.com>
2019-12-27 19:57:35 +0800

    12

---
+++ js/touchEvent.js
@@ -0,0 +1,27 @@
+// .wrap:hover .front {
+//     transform: translateZ(200px);
+// }
+
+// .wrap:hover .back {
+//     transform: translateZ(-200px) rotateY(180deg);
+// }
+
+// .wrap:hover .right {
+//     transform: rotateY(-270deg) translateZ(100px) translateX(100px);
+// }
+
+// .wrap:hover .left {
+//     transform: rotateY(270deg) translateZ(100px) translateX(-100px);
+// }
+
+// .wrap:hover .top {
+//     transform: rotateX(-270deg) translateZ(100px) translateY(-100px);
+// }
+
+// .wrap:hover .bottom {
+//     transform: rotateX(270deg) translateZ(100px) translateY(100px);
+// }
+
+$('.wrap').bind('touchstart',function(){
+    alert(520)
+})
